29acf43f267e591d7c864c2cbf80388bda593dfd
Adityarayyy <adityasray46@gmail.com>
2025-09-05 04:15:51 +0530

    ui changes done

--- calendly-clone-landing/src/main.jsx
+++ calendly-clone-landing/src/main.jsx
@@ -17,12 +17,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.jsx';
+
 import './index.css';
+import SchedulePage from './components/SchedulePage.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="165982500731-too3mr5lrt1205dpjrqup05qdd1s83nc.apps.googleusercontent.com">
       <BrowserRouter>
+      {/* <SchedulePage/> */}
         <App />
       </BrowserRouter>
     </GoogleOAuthProvider>
